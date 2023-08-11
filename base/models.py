e9f7fab6b77266946f71d4d98650dfdad9ad9111
RickVerbon <rick89@gmail.com>
2023-08-11 12:28:09 +0200

    Added models that we are going to need.

--- base/models.py
+++ base/models.py
@@ -1,2 +1,33 @@
 from django.db import models
+from users.models import Profile
+
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Question(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    question_title = models.CharField(max_length=200)
+    question_text = models.TextField(max_length=2000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    upvotes = models.IntegerField()
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="comments_written")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Upvote(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="upvote")
+    post = models.ForeignKey(Question, on_delete=models.CASCADE)  # Or Comment, depending on your use case
+
+    class Meta:
+        unique_together = ['user', 'post']  # Enforces one upvote per user per post
